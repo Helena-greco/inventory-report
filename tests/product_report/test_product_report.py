0e91c7d643d3c3b31e3f66f11e2940df32356b11
Helena <nenafgreco@hotmail.com>
2022-06-17 17:48:13 -0300

    requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(1, "miojo", "nissan lámen", "25/12/2019", "25/12/2023",
+        "2512190001", "Armazenar em ambiente seco")
+
+    assert produto.__repr__() == (
+        "O produto miojo fabricado em 25/12/2019"
+        " por nissan lámen com validade até 25/12/2023"
+        " precisa ser armazenado Armazenar em ambiente seco."
+    )
+    pass
