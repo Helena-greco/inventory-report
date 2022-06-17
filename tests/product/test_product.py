59693c0dbc63d4cb12ebef34da22a61679fd4bf2
Helena <nenafgreco@hotmail.com>
2022-06-17 18:01:56 -0300

    corrigindo erro novamente

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -3,7 +3,7 @@ from inventory_report.inventory.product import Product
 
 def test_cria_produto():
     produto = Product(1, "miojo", "nissan lámen", "25/12/2019", "25/12/2023",
-                    "2512190001", "Armazenar em ambiente seco")
+                     "2512190001", "Armazenar em ambiente seco")
 
     assert produto.id == 1
     assert produto.nome_do_produto == "miojo"
